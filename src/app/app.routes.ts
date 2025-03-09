69fd3e95be4d16b2d1c22946bc6f76817481f888
yunusyerli1 <yunusyerli1@gmail.com>
2025-03-09 19:21:26 +0300

    Set custom scss structure and add bootstrap

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './views/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "404", component: NotFoundComponent },
+  { path: "**", component: NotFoundComponent },
+];
